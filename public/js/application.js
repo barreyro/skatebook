4736ac11df484e3aa492b5238959e999094c49b8
Stephanie <stephaniebarreyro@gmail.com>
2015-06-01 22:41:07 -0700

    Change JS fade in times.

--- public/js/application.js
+++ public/js/application.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-
     //carousel
     var listItems = $("#items").children('li'),
         activeImageItems = $('#selector').children('li'),
@@ -13,14 +12,14 @@ $( document ).ready(function() {
             i = (current < listLength - 1) ? (current + 1) : 0;
         }
         activeImageItems.removeClass('active').eq(i).addClass('active');
-        listItems.fadeOut(400).eq(i).fadeIn(400);
+        listItems.fadeOut(500).eq(i).fadeIn(50);
 
         current = i;
 
         clearTimeout(changeTimeout);
         changeTimeout = setTimeout(function() {
             move('next');
-        }, 4000);
+        }, 5000);
     };
 
     // clickable circle
@@ -33,22 +32,23 @@ $( document ).ready(function() {
     move('next');
 
     $("#home_link").click(function() {
-        $("#center_carousel").children().hide();
-        $("#home_view").fadeIn(1000);
+        $("#center_view").children().hide();
+        $("#home_view").fadeIn(100);
     });
 
-    $("#video_link").click(function() {
-        $("#center_carousel").children().hide();
-        $("#video_view").fadeIn(1000);
-    });
 
     $("#pictures_link").click(function() {
-        $("#center_carousel").children().hide();
-        $("#carousel").fadeIn(1000);
+        $("#center_view").children().hide();
+        $("#carousel").fadeIn(100);
     });
 
     $("#music_link").click(function() {
-        $("#center_carousel").children().hide();
-        $("#music_view").fadeIn(1000);
+        $("#center_view").children().hide();
+        $("#music_view").fadeIn(100);
+    });
+
+    $("#video_link").click(function() {
+        $("#center_view").children().hide();
+        $("#video_view").fadeIn(100);
     });
 });
